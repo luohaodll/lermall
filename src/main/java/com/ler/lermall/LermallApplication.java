f2aaca44174bb851a15af32294ef64524559189c
luohao <luoh@travelsky.com>
2019-08-30 10:51:53 +0800

    Initial commit

---
+++ src/main/java/com/ler/lermall/LermallApplication.java
@@ -0,0 +1,13 @@
+package com.ler.lermall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LermallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LermallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ler/lermall/LermallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ler.lermall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LermallApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
