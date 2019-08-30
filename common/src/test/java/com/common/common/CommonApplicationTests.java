7401e1be3a28f06e97378914a23fe69c31fce100
luohao <luoh@travelsky.com>
2019-08-30 16:44:20 +0800

    lermall

--- common/src/test/java/com/common/common/CommonApplicationTests.java
+++ common/src/test/java/com/common/common/CommonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ler.lermall;
+package com.common.common;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LermallApplicationTests {
+public class CommonApplicationTests {
 
     @Test
     public void contextLoads() {
--- manager/src/main/java/com/manager/controller/ManagerApplication.java
+++ manager/src/main/java/com/manager/controller/ManagerApplication.java
@@ -1,13 +1,13 @@
-package com.ler.lermall;
+package com.manager.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LermallApplication {
+public class ManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LermallApplication.class, args);
+        SpringApplication.run(ManagerApplication.class, args);
     }
 
 }
