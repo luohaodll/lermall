0ab952bdf73eb35ddd34b9c7499d3a4892fd97a3
luohao <luoh@travelsky.com>
2019-09-11 10:40:21 +0800

    lermall

--- manager/src/main/java/com/manager/ManagerApplication.java
+++ manager/src/main/java/com/manager/ManagerApplication.java
@@ -1,9 +1,11 @@
-package com.manager.controller;
+package com.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.manager.dao")
 public class ManagerApplication {
 
     public static void main(String[] args) {
